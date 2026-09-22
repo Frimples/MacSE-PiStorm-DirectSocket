fdfc34f9980b6b7ff16153f21cba5c8dc80dfb47
Frimples <Frimples@users.noreply.github.com>
2026-09-21 19:07:41 -0500

    Package tested Mac SE direct-socket RTL artifact

---
+++ run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eu
+ROOT=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+iverilog -g2012 -Wall -s tb -o "$ROOT/macse_arbitration_test.vvp" "$ROOT/pistorm.v" "$ROOT/tb_arbitration.v"
+vvp "$ROOT/macse_arbitration_test.vvp"
+iverilog -g2012 -Wall -s tb_timing -o "$ROOT/macse_timing_test.vvp" "$ROOT/pistorm.v" "$ROOT/tb_timing.v"
+vvp "$ROOT/macse_timing_test.vvp"
+printf '%s\n' 'ALL RTL TESTS PASSED'
